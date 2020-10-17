9ee5bd66b9ee0b3590efc9347ad08330ab319a86
Nischay Sharma <59340190+Sscorp9412@users.noreply.github.com>
2020-10-17 23:52:11 +0530

    Solution of problem Even Sum in python (#9)
    Added the solution of problem Even Sum in Python3
    
    fixes #8

---
+++ Mathematics/number_theory/even_sum/Python3/0001_solution.py
@@ -0,0 +1,28 @@
+"""
+Solution of problem 'Even Sum'.
+@file 0001_solution.py
+@author Nischay Sharma
+@version 1.0
+"""
+
+def even_sum(n):
+    """Calculate the sum of even number in the problem 'Even_sum'
+
+    Parameters
+    ----------
+    n : int
+
+    Returns
+    ------
+    int
+        sum of even numbers
+    """
+    result = 0
+    for i in range(2, num+1):
+        if i % 2 == 0:
+            result + = i 
+    return result
+
+if __name__ == "__main__":
+    num=int(input())
+    print(even_sum(num))
